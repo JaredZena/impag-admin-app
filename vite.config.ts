7ab9fc93378004adba716f8c00764541c5dcd14e
JaredZena <zena.hernandez.010195@gmail.com>
2025-02-19 22:42:53 -0600

    Fix Vercel deployment issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import path from 'path';
+import path from "node:path";
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react-swc";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   server: {
@@ -16,7 +20,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
