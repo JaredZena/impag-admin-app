a849f70572502331ffbe16704279bb403ec68fc4
JaredZena <zena.hernandez.010195@gmail.com>
2025-07-26 00:34:15 -0500

    redirecting

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 export default defineConfig(({ mode }) => ({
   server: mode === "production" ? {
     proxy: {
-      "/api": {
+      "/": {
         target: "https://democratic-cuckoo-impag-f0717e14.koyeb.app",
         changeOrigin: true,
         secure: false,
